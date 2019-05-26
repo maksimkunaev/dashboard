7a82a98e7bd741022b52e8fe084c567fee062efd
Maksim Kunaev <maksim.kunaev@latoken.com>
2019-05-26 03:02:13 +0300

    add table

--- src/components/index.js
+++ src/components/index.js
@@ -5,6 +5,9 @@ import Charts from "./Charts";
 import container from "./container";
 import { bind } from 'decko';
 import 'antd/dist/antd.css';
+import moment from 'moment';
+import { DatePicker } from 'antd';
+const { RangePicker } = DatePicker;
 
 class App extends Component {
   componentDidMount() {
@@ -17,15 +20,27 @@ class App extends Component {
     this.props.getChartsData();
   }
 
+  @bind
+  onChange(dates, dateStrings) {
+      console.log(dateStrings)
+  }
+
   render() {
     const { data: { table, charts }, currentConfig } = this.props;
-
+    const dateFormat = 'YYYY-MM-DD';
     return (
         <div className={s.app}>
+
           <div className={s.container}>
             <Table className={s.item} data={table} config={currentConfig} />
             <Charts className={s.item} data={charts} config={currentConfig} />
           </div>
+
+          <RangePicker
+            defaultValue={[moment('2015/01/01', dateFormat), moment('2015/01/01', dateFormat)]}
+            format={dateFormat}
+            onChange={this.onChange}
+          />
         </div>
       );
   }
